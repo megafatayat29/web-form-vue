b756f5c5d2d64233de4df3fc3568d57116274e2e
itbsp16 [Mega.Fatayat] <P041121007@bussan.co.id>
2023-09-16 13:02:47 +0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignUpFormVue/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignUpFormVue from './components/SignUpForm.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: { SignUpFormVue }
 }
 </script>
 
@@ -22,5 +19,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+body {
+  margin: 0;
+  background: #eee;
 }
 </style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,135 @@
+<template>
+  <form>
+    <label>Nama:</label>
+    <input type="text" required v-model="nama">
+
+    <label>NIK:</label>
+    <input type="number" required v-model="nik">
+
+    <label>Nomor Kartu Keluarga:</label>
+    <input type="number" required v-model="noKk">
+
+    <label>Foto KTP:</label>
+    <input type="file" required>
+
+    <label>Foto Kartu Keluarga:</label>
+    <input type="file" required>
+
+    <label>Umur:</label>
+    <input type="number" required v-model="umur">
+
+    <label>Jenis Kelamin:</label>
+    <select v-model="jenisKelamin">
+      <option value="0">Laki-laki</option>
+      <option value="1">Perempuan</option>
+    </select>
+
+    <label>Provinsi:</label>
+    <input type="text" required v-model="provinsi">
+
+    <label>Kab/Kota:</label>
+    <input type="text" required v-model="kabupaten">
+
+    <label>Kecamatan:</label>
+    <input type="text" required v-model="kecamatan">
+
+    <label>Kelurahan/Desa:</label>
+    <input type="text" required v-model="kelurahan">
+
+    <label>Alamat:</label>
+    <input type="text" required v-model="alamat">
+
+    <label>RT:</label>
+    <input type="number" required v-model="rt">
+
+    <label>RW:</label>
+    <input type="number" required v-model="rw">
+
+    <label>Penghasilan sebelum pandemi:</label>
+    <input type="number" required v-model="penghasilanSebelumPandemi">
+
+    <label>Penghasilan setelah pandemi:</label>
+    <input type="number" required v-model="penghasilanSetelahPandemi">
+
+    <label>Alasan membutuhkan bantuan:</label>
+    <input type="number" required v-model="alasan">
+
+    <div class="terms">
+      <label><span>Saya menyatakan bahwa data yang diisikan adalah benar dan siap mempertanggungjawabkan apabila ditemukan ketidaksesuaian dalam data tersebut.</span></label>
+    </div>
+
+    <button>Submit</button>
+  </form>
+  <p>Jenis kelamin: {{ jenisKelamin }}</p>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nama: '',
+      nik: '',
+      noKk: '',
+      umur: '',
+      jenisKelamin: '',
+      provinsi: '',
+      kabupaten: '',
+      kecamatan: '',
+      kelurahan: '',
+      alamat: '',
+      rt: '',
+      rw: '',
+      penghasilanSebelumPandemi: '',
+      penghasilanSetelahPandemi: '',
+      alasan: ''
+    }
+  }
+}
+</script>
+
+<style>
+  form {
+    max-width: 420px;
+    margin: 30px auto;
+    background: white;
+    text-align: left;
+    padding: 40px;
+    border-radius: 10px;
+  }
+  label {
+    color: #aaa;
+    display: inline-block;
+    margin: 25px 0 15px;
+    font-size: 0.6em;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    font-weight: bold;
+  }
+  input, select {
+    display: block;
+    padding: 10px 6px;
+    width: 100%;
+    box-sizing: border-box;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    color: #555;
+  }
+  button {
+    background: #0faf87;
+    color: white;
+    border: none;
+    padding: 8px 16px;
+    border-radius: 4px;
+    font-size: 16px;
+    letter-spacing: 1px;
+    cursor: pointer;
+    margin: 10px;
+  }
+  button[disabled] {
+    opacity: 0.2;
+    cursor: not-allowed;
+  }
+  span {
+    color: #ec5656;
+  }
+</style>

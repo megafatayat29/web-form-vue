052e2e15f8b25931ff1fb56e473ba8647469a766
itbsp16 [Mega.Fatayat] <P041121007@bussan.co.id>
2023-09-16 22:06:03 +0700

    feat - Handle submit, display modal

--- src/components/SignUpForm.vue
+++ src/components/SignUpForm.vue
@@ -1,5 +1,15 @@
 <template>
-  <form>
+  <div v-if="showModal">
+    <Modal @close="toggleModal">
+      <template v-slot:details>
+        <h1>Success!</h1>
+        <p>Your data has been sent to system ^^</p>
+        <a href="#">Close</a>
+      </template>
+    </Modal>
+  </div>
+  <form @submit.prevent="handleSubmit">
+    <h2>Registrasi Penerima Bantuan Sosial</h2>
     <label>Nama:</label>
     <input type="text" required v-model="nama">
 
@@ -20,24 +30,37 @@
 
     <label>Jenis Kelamin:</label>
     <select v-model="jenisKelamin">
+      <!-- <option v-for="item in JenisKelaminEnum" :key="item">{{ item }}</option> -->
       <option value="0">Laki-laki</option>
       <option value="1">Perempuan</option>
     </select>
 
     <label>Provinsi:</label>
-    <input type="text" required v-model="provinsi">
+    <select v-model="provinsi">
+      <option value="0">Jakarta Selatan</option>
+      <option value="1">Jakarta Barat</option>
+    </select>
 
     <label>Kab/Kota:</label>
-    <input type="text" required v-model="kabupaten">
+    <select v-model="kabupaten">
+      <option value="0">Jakarta Selatan</option>
+      <option value="1">Jakarta Barat</option>
+    </select>
 
     <label>Kecamatan:</label>
-    <input type="text" required v-model="kecamatan">
+    <select v-model="kecamatan">
+      <option value="0">Jakarta Selatan</option>
+      <option value="1">Jakarta Barat</option>
+    </select>
 
     <label>Kelurahan/Desa:</label>
-    <input type="text" required v-model="kelurahan">
+    <select v-model="kelurahan">
+      <option value="0">Jakarta Selatan</option>
+      <option value="1">Jakarta Barat</option>
+    </select>
 
     <label>Alamat:</label>
-    <input type="text" required v-model="alamat">
+    <textarea required v-model="alamat"></textarea>
 
     <label>RT:</label>
     <input type="number" required v-model="rt">
@@ -52,18 +75,22 @@
     <input type="number" required v-model="penghasilanSetelahPandemi">
 
     <label>Alasan membutuhkan bantuan:</label>
-    <input type="number" required v-model="alasan">
+    <input type="text" required v-model="alasan">
 
-    <div class="terms">
-      <label><span>Saya menyatakan bahwa data yang diisikan adalah benar dan siap mempertanggungjawabkan apabila ditemukan ketidaksesuaian dalam data tersebut.</span></label>
+    <div class="container">
+      <input type="checkbox" v-model="terms" required>
+      <label>Saya menyatakan bahwa data yang diisikan adalah benar dan siap mempertanggungjawabkan apabila ditemukan ketidaksesuaian dalam data tersebut.</label>
     </div>
 
-    <button>Submit</button>
+    <div class="submit">
+      <button>Register</button>
+    </div>
   </form>
-  <p>Jenis kelamin: {{ jenisKelamin }}</p>
 </template>
 
 <script>
+import Modal from './Modal.vue'
+
 export default {
   data() {
     return {
@@ -81,8 +108,19 @@ export default {
       rw: '',
       penghasilanSebelumPandemi: '',
       penghasilanSetelahPandemi: '',
-      alasan: ''
+      alasan: '',
+      terms: false,
+      showModal: false
     }
+  },
+  components: { Modal },
+  methods: {
+    handleSubmit() {
+      this.showModal = !this.showModal
+    },
+    toggleModal() {
+      this.showModal = !this.showModal
+    },
   }
 }
 </script>
@@ -105,7 +143,7 @@ export default {
     letter-spacing: 1px;
     font-weight: bold;
   }
-  input, select {
+  input, select, textarea {
     display: block;
     padding: 10px 6px;
     width: 100%;
@@ -114,22 +152,44 @@ export default {
     border-bottom: 1px solid #ddd;
     color: #555;
   }
+  .container {
+    display: flex;
+    flex-direction: row;
+    place-items: flex-start baseline;
+    align-items: baseline;
+    text-align: justify;
+    text-justify: inter-word;
+  }
   button {
-    background: #0faf87;
+    background: #0b6dff;
+    border: 0;
+    padding: 10px 20px;
+    margin-top: 20px;
     color: white;
+    border-radius: 20px;
+  }
+  .submit {
+    text-align: center;
+  }
+  input[type=file]::file-selector-button {
+    margin-right: 20px;
     border: none;
+    background: #084cdf;
     padding: 8px 16px;
     border-radius: 4px;
-    font-size: 16px;
-    letter-spacing: 1px;
+    color: #fff;
     cursor: pointer;
-    margin: 10px;
+    transition: background .2s ease-in-out;
   }
-  button[disabled] {
-    opacity: 0.2;
-    cursor: not-allowed;
+  input[type=file]::file-selector-button:hover {
+    background: #0d45a5;
   }
-  span {
-    color: #ec5656;
+  select {
+    appearance: none;
+    -webkit-appearance: none;
+    font-size: 1em;
+    padding: 0.675em 6em 0.675em 1em;
+    background-color: #fff;
+    cursor: pointer;
   }
 </style>
